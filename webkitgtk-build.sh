238913a66c2c6916ed2e44f2f435ee417ece1289
Daniel Massey <masseyd1396@gmail.com>
2022-08-12 20:23:12 +0100

    Upgrade to WebKitGTK 2.36.6

--- webkitgtk-build.sh
+++ webkitgtk-build.sh
@@ -23,10 +23,11 @@ mkdir webkitgtk-build; cd webkitgtk-build
 cmake -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_SKIP_RPATH=ON -DPORT=GTK -DLIB_INSTALL_DIR=/usr/lib -DENABLE_GAMEPAD=OFF -DENABLE_GLES2=ON -DENABLE_GTKDOC=ON -DENABLE_MINIBROWSER=ON -DUSE_ANGLE_WEBGL=OFF -DUSE_AVIF=OFF -DUSE_GTK4=OFF -DUSE_JPEGXL=OFF -DUSE_LIBHYPHEN=OFF -DUSE_SOUP2=ON -DUSE_WOFF2=ON -DUSE_WPE_RENDERER=ON -Wno-dev -G Ninja ..
 ninja -j$(nproc)
 DESTDIR="${workdir}"/out ninja install
-install -dm755 "${workdir}"/usr/share/licenses/webkitgtk
-find ../Source -name 'COPYING*' -or -name 'LICENSE*' -print0 | sort -z | while IFS= read -d $'\0' -r _f; do echo "### $_f ###"; cat "$_f"; echo; done > "${workdir}"/usr/share/licenses/webkitgtk/LICENSE
+install -dm755 "${workdir}"/out/usr/share/licenses/webkitgtk
+find ../Source -name 'COPYING*' -or -name 'LICENSE*' -print0 | sort -z | while IFS= read -d $'\0' -r _f; do echo "### $_f ###"; cat "$_f"; echo; done > "${workdir}"/out/usr/share/licenses/webkitgtk/LICENSE
 
 cd "${workdir}"/out
+find . -type f -exec strip --strip-unneeded {} ';' 2>/dev/null || true
 tar -cJf "${savedir}/webkitgtk-${WEBKITGTK_VERSION}-MassOS${MASSOS_RELEASE}-icu${ICU_VER}-$(uname -m).tar.xz" *
 cd "${savedir}"
 rm -rf "${workdir}"
